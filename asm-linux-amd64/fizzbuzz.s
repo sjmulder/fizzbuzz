349237fd7012fe311b42f409240cbd0443d63617
Sijmen J. Mulder <ik@sjmulder.nl>
2019-07-10 01:11:13 +0200

    Use write() syscall for all except numeric case

--- asm-linux-amd64/fizzbuzz.s
+++ asm-linux-amd64/fizzbuzz.s
@@ -1,50 +1,57 @@
+%define	WRITE	1
+%define STDOUT	1
+
 	global	main
 	extern	printf
 
 	section	.text
-main:	enter	0, 0
-	push	rbx
+main:	enter	10, 0
+	mov	[rbp-8], rbx
+	mov	byte [rbp-9], 3		; next fizz
+	mov	byte [rbp-10], 5	; next buzz
 	xor	ebx, ebx
 .loop:	inc	ebx
 	cmp	ebx, 101
 	je	.done
-	cmp	ebx, [i_nf]
+	cmp	bl, [rbp-9]
 	je	.fizz
-	cmp	ebx, [i_nb]
+	cmp	bl, [rbp-10]
 	je	.buzz
 	mov	edi, s_num
 	mov	esi, ebx
 	xor	eax, eax
 	call	printf
 	jmp	.loop
-.buzz:	add	dword [i_nb], 5
-	mov	edi, s_buzz
-	xor	eax, eax
-	call	printf
+.buzz:	add	byte [rbp-10], 5
+	mov	eax, WRITE
+	mov	edi, STDOUT
+	mov	esi, s_buzz
+	mov	edx, 5
+	syscall
 	jmp	.loop
-.fizz:	add	dword [i_nf], 3
-	cmp	ebx, [i_nb]
+.fizz:	add	byte [rbp-9], 3
+	cmp	bl, [rbp-10]
 	je	.fibu
-	mov	edi, s_fizz
-	xor	eax, eax
-	call	printf
+	mov	eax, WRITE
+	mov	edi, STDOUT
+	mov	esi, s_fizz
+	mov	edx, 5
+	syscall
 	jmp	.loop
-.fibu:	add	dword [i_nb], 5
-	mov	edi, s_fibu
-	xor	eax, eax
-	call	printf
+.fibu:	add	byte [rbp-10], 5
+	mov	eax, WRITE
+	mov	edi, STDOUT
+	mov	esi, s_fibu
+	mov	edx, 9
+	syscall
 	jmp	.loop
 .done:	xor	eax, eax
-	pop	rbx
+	mov	rbx, [rbp-8]
 	leave
 	ret
 
-	section .data
-i_nf:	dd	3		; next fizz
-i_nb:	dd	5		; next buzz
-
 	section .rodata
-s_fizz:	db	"Fizz", 10, 0
-s_buzz:	db	"Buzz", 10, 0
-s_fibu:	db	"FizzBuzz", 10, 0
+s_fizz:	db	"Fizz", 10
+s_buzz:	db	"Buzz", 10
+s_fibu:	db	"FizzBuzz", 10
 s_num:	db	"%d", 10, 0
