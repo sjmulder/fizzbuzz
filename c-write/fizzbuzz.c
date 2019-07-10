aa6959548ce899cdc9c0bb080d4695ed24109bf2
Sijmen J. Mulder <ik@sjmulder.nl>
2019-07-10 10:31:10 +0200

    Count decimally instead of converting every time

--- c-write/fizzbuzz.c
+++ c-write/fizzbuzz.c
@@ -1,13 +1,19 @@
 #include <unistd.h>
 
-static void write_int(int);
-
 int
 main()
 {
-	int i;
+	char str[4] = "  0\n";
+	int i, j;
 
 	for (i = 1; i <= 100; i++) {
+		for (j = 2; str[j] == '9'; j--)
+			str[j] = '0';
+		if (str[j] == ' ')
+			str[j] = '1';
+		else
+			str[j]++;
+
 		if (i % 3 == 0 && i % 5 == 0)
 			write(1, "FizzBuzz\n", 9);
 		else if (i % 3 == 0)
@@ -15,23 +21,8 @@ main()
 		else if (i % 5 == 0)
 			write(1, "Buzz\n", 5);
 		else
-			write_int(i);
+			write(1, str, 4);
 	}
 
 	return 0;
-}
-
-static void
-write_int(int num)
-{
-	char buf[4];
-	int i = 3;
-
-	while (i > 0 && num) {
-		buf[--i] = (char)('0' + num % 10);
-		num /= 10;
-	}
-
-	buf[3] = '\n';
-	write(1, &buf[i], 4-i);
 }
